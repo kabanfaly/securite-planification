7a95c21269421889e97a40f507ddb08aa7ad4fd1
Kaba <kaba@kaba.(none)>
2013-12-09 01:05:28 +0100

    creating models

---
+++ application/controllers/agent.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Agent controllers
+ * @author N'faly KABA
+ * @since   08/12/2013
+ * @version 2.0
+ * @copyright KABANFALY
+ * @package plannification
+ * @subpackage application/controllers/
+ * @filesource  agent.php
+ */
+class Agent extends CI_Controller
+{
+    
+}
