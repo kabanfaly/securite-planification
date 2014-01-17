d8ecd454cacde591b47d0c6900879a0e43ac76d3
Kaba <kaba@kaba.(none)>
2014-01-17 23:02:59 +0100

    adding jquery-ui and setting page design

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,10 @@
-  <?php
-        // put your code here
-        ?>
-    </body>
+<?php
+// put your code here
+?>
+  </div>
+        </div>
+<div id="footer">
+    <center><small>&copy; KABA N'faly</small></center>
+</div>
+</body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -3,9 +3,20 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>css/jquery/jquery-ui.css" >
         <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>css/style.css" >
         <script src="<?php echo base_url(); ?>js/jquery/jquery.js"></script>
         <script src="<?php echo base_url(); ?>js/knockout.js"></script>
         <script src="<?php echo base_url(); ?>js/script.js"></script>
     </head>
     <body>
+        <div id="header">
+
+        </div>
+        <div id="content">
+            <div id="leftMenu">
+
+            </div>
+            <div id="mainContent">
+
+          
