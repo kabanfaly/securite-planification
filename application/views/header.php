3ab6d8d231cc039e694273da09f2e4b8f8d3563b
kaba <kaba@kaba>
2014-03-24 22:34:19 +0100

    cleaning

--- application/views/header.php
+++ application/views/header.php
@@ -6,7 +6,8 @@
         <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>css/jquery/jquery-ui.css" >
         <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>css/style.css" >
         <script src="<?php echo base_url(); ?>js/jquery/jquery.js"></script>
-        <script src="<?php echo base_url(); ?>js/knockout.js"></script>
+        <script src="<?php echo base_url(); ?>js/jquery/jquery-ui.min.js"></script>
+        <script src="<?php echo base_url(); ?>js/knockout/knockout.js"></script>
         <script src="<?php echo base_url(); ?>js/script.js"></script>
     </head>
     <body>
@@ -15,8 +16,19 @@
         </div>
         <div id="content">
             <div id="leftMenu">
-
+                <ul>
+                    <li><span id="add_tab">Gestion des agents</span></li>
+                    <li><span id="add_tab">Gestion des sites</span></li>
+                    <li><span id="add_tab">Gestion des ville</span></li>
+                </ul>
             </div>
             <div id="mainContent">
+                <div id="tabs">
+                    <ul>
+                        <!--<li><a href="#tabs-1">Gestion des agents</a> <span class="ui-icon ui-icon-close" role="presentation">Remove Tab</span></li>-->
+                    </ul>
+                    <div id="tabs-1">
+                        <p></p>
+                    </div>
+                </div>
 
-          
--- application/views/site/index.php
+++ application/views/site/index.php
@@ -1,15 +1,1 @@
 Gestion des sites
-<?php
-    function incrementation()
-    {
-        static $i = 5; // Si c'est le premier appel de la fonction, initialisation de la variable.
-        $i++;
-        echo $i;
-    }
-
-    incrementation(); // Affiche « 1 ».
-    incrementation(); // Affiche « 2 ».
-    incrementation(); // Affiche « 3 ».
-    incrementation(); // Affiche « 4 ».
-    incrementation(); // Affiche « 5 ».
-?>
